2414241db9d51fcb52e9e8255ae2d6c0e9efb5dd
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2018-11-24 00:29:35 +0100

    Implement that fully fledged

--- index.ts
+++ index.ts
@@ -1,21 +1,29 @@
+const FG_YELLOW = '\x1b[33m';
+const FG_BLUE = "\x1b[34m"
+const RESET = "\x1b[0m";
+const line = '*'.repeat(20);
 export interface Observer {
-    notify(msg: string): void;
+    notify(value?: string, subject?: Subject): void;
     getName(): string;
 }
 
 class Watcher implements Observer {
+ 
     constructor(private name: string){}
 
-    public notify(msg: string): void {
-        console.log(this.name + " notified." + " new " + msg);
+    public notify(value?: string, subject?: Subject): void {
+        console.log("\n" + FG_YELLOW + this.name + 'received : ' + RESET + " " + value + "\n",
+        FG_YELLOW + " from :" + RESET , subject , "\n\n" + line);
     }
 
     public getName(): string {
         return this.name;
     }
+
 }
 
 class Subject {
+
     private observers: Observer[] = [];
 
     public append(observer: Observer): void {
@@ -24,7 +32,7 @@ class Subject {
 
     public remove(observer: Observer): void {
         let index: number = this.observers.indexOf(observer);
-        if(index > -1){
+        if(~index){
             let removedObserver: Observer = this.observers.splice(index, 1)[0];
             console.log('removed ' + removedObserver.getName());
         } else {
@@ -32,9 +40,10 @@ class Subject {
         }
     }
 
-    public next(msg: string): void {
-        this.observers.forEach( observer => observer.notify(msg))
+    public next(msg?: string): void {
+        this.observers.forEach( observer => observer.notify(msg, this));
     }
+    
 }
 
 class IMBD extends Subject {
@@ -47,11 +56,23 @@ class IMBD extends Subject {
 
 }
 
+class NewsPublisher extends Subject {
+    private newsList: string[] = [];
+
+    public add(movie: string): void {
+        this.newsList.push(movie);
+        this.next(movie);
+    }
+
+}
+
 
 let milad : Watcher = new Watcher('Milad');
 
 let imbd: IMBD = new IMBD();
+let newsPublisher: NewsPublisher = new NewsPublisher();
 imbd.append(milad);
+newsPublisher.append(milad);
 
 imbd.add("Die hard");
 imbd.add("Matrix");
@@ -62,6 +83,7 @@ imbd.add("Gone");
 
 let szandi : Watcher = new Watcher('Szandi');
 imbd.append(szandi);
+newsPublisher.append(szandi);
 imbd.add("Endless love");
 
 imbd.remove(milad);
@@ -70,3 +92,10 @@ imbd.add("Matrix II");
 
 
 
+
+newsPublisher.add("Tomorrow will be raining.");
+imbd.add("Bad Ass");
+imbd.add("Matrix III");
+newsPublisher.add("Github is being acquired by Microsoft");
+imbd.add("Inspection");
+newsPublisher.add("an accident had happened.");
